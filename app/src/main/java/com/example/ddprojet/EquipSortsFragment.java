e1f71aa958b515a13d383aecb43987b479efe27c
DulcheE <eddy.dulche@live.fr>
2020-02-08 19:32:06 +0100

    Navigation Bar

---
+++ app/src/main/java/com/example/ddprojet/EquipSortsFragment.java
@@ -0,0 +1,19 @@
+package com.example.ddprojet;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class EquipSortsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.equip_sorts_layout, container, false);
+    }
+}
--- app/src/main/java/com/example/ddprojet/MainActivity.java
+++ app/src/main/java/com/example/ddprojet/MainActivity.java
@@ -1,14 +1,101 @@
 package com.example.ddprojet;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        this.drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, this.drawerLayout, toolbar, R.string.open_actionBar, R.string.close_actionBar);
+        this.drawerLayout.addDrawerListener(toggle);
+
+        toggle.syncState();
+
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new DescriptionFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_description);
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.nav_description :
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new DescriptionFragment()).commit();
+
+                break;
+
+            case R.id.nav_caracComp :
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new CaracCompFragment()).commit();
+
+                break;
+
+            case R.id.nav_capaDons :
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new CapaDonsFragment()).commit();
+
+                break;
+
+            case R.id.nav_equipSorts :
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new EquipSortsFragment()).commit();
+
+                break;
+
+            case R.id.nav_personnalité :
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new PersonnaliteFragment()).commit();
+
+                break;
+        }
+
+        this.drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if(this.drawerLayout.isDrawerOpen(GravityCompat.START))
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
+
     }
 }
