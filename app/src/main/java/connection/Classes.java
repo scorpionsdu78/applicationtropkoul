8c964843038ca19998b7c2e5c038201b23d36cb3
scorpionsdu78 <fboni.78@gmail.com>
2020-02-09 13:00:46 +0100

    constructeur classe fini

--- app/src/main/java/connection/Classes.java
+++ app/src/main/java/connection/Classes.java
@@ -10,7 +10,11 @@ import java.util.List;
 
 public class Classes extends APIconnection {
 
-    private List<ProficienciesList> choice;
+    private List<ProficienciesList> ProficienciesChoice;
+    private ProficienciesList BasicProficiencies;
+    private String name;
+    private int hitDice;
+    private List<String> JetDeSauv;
 
     public Classes() throws IOException, JSONException{
         super("http://www.dnd5eapi.co/api/classes/");
@@ -19,29 +23,60 @@ public class Classes extends APIconnection {
     public Classes(String classe) throws IOException, JSONException {
         super( "http://www.dnd5eapi.co/api/classes/" + classe);
 
-        choice = new ArrayList<>();
+        //creations de la proficiencies l
+        ProficienciesChoice = new ArrayList<>();
 
-        JSONArray proficienciesChoice = file.getJSONArray("proficiency_choices");
+        //recuperation des tableau de proficiencies choice
+        JSONArray proficienciesChoiceTmp = file.getJSONArray("proficiency_choices");
 
-        for (int i=0; i<proficienciesChoice.length(); i++){
-            JSONObject Proficiancy = proficienciesChoice.getJSONObject(i);
+        //on parcour la liste des different tableau de proficiencies choice
+        for (int i=0; i<proficienciesChoiceTmp.length(); i++){
+            //on récupere la liste
+            JSONObject ProficianciTmp = proficienciesChoiceTmp.getJSONObject(i);
 
-            int size = Proficiancy.getInt("choose");
-            JSONArray ListOfChoice = Proficiancy.getJSONArray("from");
-
+            //on récupère les attribut choose et la liste de proficiencies
+            int size = ProficianciTmp.getInt("choose");
+            JSONArray ListOfChoice = ProficianciTmp.getJSONArray("from");
+            //on crer un objet proficiencies list
             ProficienciesList result = new ProficienciesList(size);
-
+            //on parcour la list des proficiencies  et on ajoute les element a l'objet
             for (int j = 0; j<ListOfChoice.length(); j++){
                 JSONObject choice = ListOfChoice.getJSONObject(j);
                 Proficiencies p = new Proficiencies(choice.getString("url"),choice.getString("name"));
                 result.add(p);
             }
 
-            choice.add(result);
+            //on ajouté le nouvel objet a la liste
+            ProficienciesChoice.add(result);
         }
+
+        //recuperation des proficiencies de base
+        BasicProficiencies = new ProficienciesList(0);
+        JSONArray BasicProficienciesTmp = file.getJSONArray("proficiencies");
+
+        for(int i = 0; i<BasicProficienciesTmp.length(); i++){
+            JSONObject tmp = BasicProficienciesTmp.getJSONObject(i);
+            BasicProficiencies.add(new Proficiencies(tmp.getString("url"),tmp.getString("name")));
+        }
+
+        //recuêration du nom
+        name = file.getString("name");
+
+        //recuperation des hit die
+        hitDice = file.getInt("hit_die");
+
+        //Recuperation des jet de sauvgarde
+        JetDeSauv = new ArrayList<>();
+        JSONArray JetSauvTmp = file.getJSONArray("saving_throws");
+        for(int i=0; i<JetSauvTmp.length(); i++){
+            JSONObject sauvegardeTmp = JetSauvTmp.getJSONObject(i);
+            JetDeSauv.add(sauvegardeTmp.getString("name"));
+        }
+
+
     }
 
     public List<ProficienciesList> getChoice() {
-        return choice;
+        return ProficienciesChoice;
     }
 }
