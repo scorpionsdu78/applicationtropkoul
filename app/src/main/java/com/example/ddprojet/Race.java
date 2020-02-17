f86fc3b5a5fa708c95de0e836f99548f06733f5b
scorpionsdu78 <fboni.78@gmail.com>
2020-02-17 17:07:12 +0100

    correction Fragment + merge code
    correction de la methode des fragment + merge avec les code précédent

---
+++ app/src/main/java/com/example/ddprojet/Race.java
@@ -0,0 +1,34 @@
+package com.example.ddprojet;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+
+import connection.APIconnection;
+import connection.Classes;
+
+public class Race extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+         View v = inflater.inflate(R.layout.description_layout, container, false);
+
+         return v;
+    }
+
+
+}
