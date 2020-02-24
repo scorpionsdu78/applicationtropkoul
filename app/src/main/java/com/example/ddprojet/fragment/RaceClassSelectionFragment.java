3859b227256d895d880f9e2e84e1b6a195ccf832
DulcheE <eddy.dulche@live.fr>
2020-02-24 17:25:18 +0100

    Update RaceClassSelectionFragment.java

--- app/src/main/java/com/example/ddprojet/fragment/RaceClassSelectionFragment.java
+++ app/src/main/java/com/example/ddprojet/fragment/RaceClassSelectionFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,22 +15,25 @@ import androidx.fragment.app.Fragment;
 import com.example.ddprojet.CharacterEditionActivity;
 import com.example.ddprojet.R;
 
+import util.ClassEnum;
 import util.CustomViewPager;
 import util.FragmentEnum;
+import util.RaceEnum;
 
 public class RaceClassSelectionFragment extends Fragment {
 
-    CharacterEditionActivity parent_activity;
+    protected CharacterEditionActivity parent_activity;
+    protected View view;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.race_class_selection_layout, container, false);
+        this.view = inflater.inflate(R.layout.race_class_selection_layout, container, false);
 
         this.parent_activity = (CharacterEditionActivity)this.getActivity();
 
-        CardView cardViewRace = v.findViewById(R.id.cardViewRace);
-        CardView cardViewClass = v.findViewById(R.id.cardViewClass);
+        CardView cardViewRace = this.view.findViewById(R.id.cardViewRace);
+        CardView cardViewClass = this.view.findViewById(R.id.cardViewClass);
 
         cardViewRace.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,7 +48,7 @@ public class RaceClassSelectionFragment extends Fragment {
             }
         });
 
-        Button buttonNext = v.findViewById(R.id.buttonNext);
+        Button buttonNext = this.view.findViewById(R.id.buttonNext);
         buttonNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -52,6 +56,31 @@ public class RaceClassSelectionFragment extends Fragment {
             }
         });
 
-        return v;
+        update();
+
+        return this.view;
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        update();
+    }
+
+    protected void update(){
+
+        String race = this.parent_activity.getCharacter().getRace();
+
+        ImageView imageViewRace = this.view.findViewById(R.id.imageViewRace);
+        imageViewRace.setImageResource(RaceEnum.getValue(race));
+
+
+        String class_ = this.parent_activity.getCharacter().getClass_();
+
+        ImageView imageViewClass = this.view.findViewById(R.id.imageViewClass);
+        imageViewClass.setImageResource(ClassEnum.getValue(class_));
+
     }
 }
